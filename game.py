97ca20dff9cd8e32824f6631f1c13d4e22915b4b
TomatoGremlin <101596047+TomatoGremlin@users.noreply.github.com>
2023-06-01 13:43:57 +0300

    refractoring loading files

--- game.py
+++ game.py
@@ -1,42 +1,14 @@
 import pygame
 from pygame.locals import *
-from pygame import mixer
 import pickle
 from os import path
 from button import*
 from gameObjects import*
 from globalVars import*
+from loadFiles import*
 
-pygame.mixer.pre_init(44100, -16, 2, 512)
-mixer.init()
 
 pygame.display.set_caption('Platformer')
-
-#load images
-sun_img = pygame.image.load('assets/sun.png')
-sun_img = pygame.transform.scale(sun_img, (TILE_SIZE, TILE_SIZE))
-bg_img = pygame.image.load('assets/sky.png')
-bg_img = pygame.transform.scale(bg_img , (SCREEN_WIDTH, SCREEN_HEIGHT))
-
-restart_img = pygame.transform.scale(pygame.image.load('assets/restart_btn.png') , (150, 50))
-start_img = pygame.transform.scale(pygame.image.load('assets/start_btn.png') , (120, 50))
-exit_img = pygame.transform.scale(pygame.image.load('assets/exit_btn.png') , (150, 50)) 
-
-
-#SOUNDS
-pygame.mixer.music.load('assets/music/gamemusic-6082.mp3')
-pygame.mixer.music.set_volume(0.2)
-pygame.mixer.music.play(-1, 0.0, 5000)
-coin_fx = pygame.mixer.Sound('assets/music/reward.mp3')
-coin_fx.set_volume(0.5)
-jump_fx = pygame.mixer.Sound('assets/music/jump.mp3')
-jump_fx.set_volume(0.3)
-game_over_fx = pygame.mixer.Sound('assets/music/beefmow.mp3')
-victory_fx =  pygame.mixer.Sound('assets/music/winsquare-6993.mp3')
-victory_fx.set_volume(0.3)
-
-
-
 
 def draw_text(text, FONT_BIG, text_col, x, y):
 	img = FONT_BIG.render(text, True, text_col)
---
+++ loadFiles.py
@@ -0,0 +1,28 @@
+import pygame
+from pygame import mixer
+from globalVars import TILE_SIZE, SCREEN_WIDTH , SCREEN_HEIGHT
+pygame.mixer.pre_init(44100, -16, 2, 512)
+mixer.init()
+
+#load images
+sun_img = pygame.image.load('assets/sun.png')
+sun_img = pygame.transform.scale(sun_img, (TILE_SIZE, TILE_SIZE))
+bg_img = pygame.image.load('assets/sky.png')
+bg_img = pygame.transform.scale(bg_img , (SCREEN_WIDTH, SCREEN_HEIGHT))
+
+restart_img = pygame.transform.scale(pygame.image.load('assets/restart_btn.png') , (150, 50))
+start_img = pygame.transform.scale(pygame.image.load('assets/start_btn.png') , (120, 50))
+exit_img = pygame.transform.scale(pygame.image.load('assets/exit_btn.png') , (150, 50)) 
+
+
+#SOUNDS
+pygame.mixer.music.load('assets/music/gamemusic-6082.mp3')
+pygame.mixer.music.set_volume(0.2)
+pygame.mixer.music.play(-1, 0.0, 5000)
+coin_fx = pygame.mixer.Sound('assets/music/reward.mp3')
+coin_fx.set_volume(0.5)
+jump_fx = pygame.mixer.Sound('assets/music/jump.mp3')
+jump_fx.set_volume(0.3)
+game_over_fx = pygame.mixer.Sound('assets/music/beefmow.mp3')
+victory_fx =  pygame.mixer.Sound('assets/music/winsquare-6993.mp3')
+victory_fx.set_volume(0.3)
